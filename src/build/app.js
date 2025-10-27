2c66afebdda979dcb6e2123fb1c1378a8319e7dd
Austin Moore <austin@austinsdk.me>
2025-10-27 18:48:07 -0500

    set up a basic express server

---
+++ src/build/app.js
@@ -0,0 +1,13 @@
+import express from "express";
+import dotenv from 'dotenv';
+// config
+dotenv.config();
+const app = express();
+var port = process.env.PORT;
+app.listen(port, e => {
+    if (e) {
+        return console.error("Failed to launch! Error:\n", e);
+    }
+    console.log(`Hosting on port ${port}! http://127.0.0.1:${port}`);
+});
+//# sourceMappingURL=app.js.map
